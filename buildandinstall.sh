16ba146526e23b8afd23c537f8578c7cc035a9b7
Jay Ray <jeremiah.ray1@gmail.com>
2015-07-15 14:03:34 -0500

    Created a Build and Install operation based on the buildall Shell Script, which moves the created Automator Actions to the appropriate folder.

---
+++ buildandinstall.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+TEMP=$(getopt -o '' -n $(basename $0) -- "$@")
+
+if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
+
+eval set -- "$TEMP"
+
+while true; do
+  case "$1" in
+    -- ) shift; break ;;
+  esac
+done
+
+RELEASEDIR="."
+
+if [ ! -d $RELEASEDIR ]
+then
+    mkdir $RELEASEDIR
+fi
+RELEASEDIR=$(python -c "import os,sys; print os.path.realpath(sys.argv[1])" $RELEASEDIR)
+
+for dir in Git*
+do
+    pushd "$dir"
+
+    if xcodebuild -target "$dir" build
+    then
+        cp -rv build/Release/"$dir".action $RELEASEDIR
+    fi
+    popd
+done
+
+mv *.action ~/Library/Automator/
